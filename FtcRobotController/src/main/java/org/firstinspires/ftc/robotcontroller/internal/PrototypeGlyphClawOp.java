0a55448a0cee300a7a103987431ec7d952d20de5
Kalvin Chang <kalvin1204@gmail.com>
2017-11-04 15:35:55 -0700

    started working on Glyph code

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/PrototypeGlyphClawOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/PrototypeGlyphClawOp.java
@@ -7,19 +7,30 @@ import com.qualcomm.robotcore.hardware.*;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- * Updated by Kalvin on 11/3/2017.
+ * Updated by Kalvin on 11/4/2017.
  * Jessica's prototype
+ * servos: leftClaw, rightClaw
+ * motor: lift
  */
 
 @TeleOp(name = "Glyph Prototype 1", group = "Default")
 public class PrototypeGlyphClawOp extends OpMode {
     //Declare any motors, servos, and sensors
-    Servo clawServo;
+    Servo leftClaw;
+    Servo rightClaw;
+    DcMotor lift;
 
     //Declare any variables & constants pertaining to specific robot mechanisms (i.e. drive train)
-    final float SERVO_MIN = 0 / 255.0f;
-    final float SERVO_MAX = 255 / 255.0f;
-    public double servoPos = 0;
+    final float SERVO_MIN_LEFT = 0 / 255.0f;
+    final float SERVO_MAX_LEFT = 255 / 255.0f;
+    final float SERVO_MIN_RIGHT = 0 / 255.0f;
+    final float SERVO_MAX_RIGHT = 255 / 255.0f;
+    final float LIFT_PWR_MAX = 0.50f;
+
+    //make sure both servos / motors are in sync
+    public double leftClawServoPos = 0;
+    public double rightClawServoPos = 0;
+    public double liftPower = 0;
 
     public PrototypeGlyphClawOp() {}
 
@@ -27,7 +38,9 @@ public class PrototypeGlyphClawOp extends OpMode {
         //Initialize motors & set direction
 
         //Initialize servos
-        clawServo = hardwareMap.servo.get("upDown");
+        leftClaw = hardwareMap.servo.get("leftClaw");
+        rightClaw = hardwareMap.servo.get("rightClaw");
+        lift = hardwareMap.dcMotor.get("lift");
 
         //Initialize sensors
 
@@ -48,22 +61,35 @@ public class PrototypeGlyphClawOp extends OpMode {
     void updateData() {
         //Add in update methods for specific robot mechanisms
         if (gamepad1.left_stick_x < 0) {
-            servoPos = (((servoPos * 255.0) + 1) / 255.0);
+            leftClawServoPos = (((leftClawServoPos * 255.0) + 1) / 255.0);
         }
         if (gamepad1.left_stick_x > 0) {
-            servoPos = (((servoPos * 255.0) - 1) / 255.0);
+            leftClawServoPos = (((leftClawServoPos * 255.0) - 1) / 255.0);
         }
+        if (gamepad1.right_stick_x < 0) {
+            rightClawServoPos = (((rightClawServoPos * 255.0) + 1) / 255.0);
+        }
+        if (gamepad1.right_stick_x > 0) {
+            rightClawServoPos = (((rightClawServoPos * 255.0) - 1) / 255.0);
+        }
+        if (gamepad2.left_stick_y == 0) {
+            liftPower = 0;
+        }
+        liftPower = -gamepad2.left_stick_y * LIFT_PWR_MAX;
     }
 
     void initialization() {
-        //Clip and Initialize Specific Robot Mechanisms
-        servoPos = Range.clip(servoPos, SERVO_MIN, SERVO_MAX);
-        clawServo.setPosition(servoPos);
+        //Clip and Initialize Specific Robot Mechanisms - initialize servo positions
+        leftClawServoPos = Range.clip(leftClawServoPos, SERVO_MIN_LEFT, SERVO_MAX_LEFT);
+        leftClaw.setPosition(leftClawServoPos);
+        rightClaw.setPosition(clawServoPos);
+        lift.setPower(liftPower);
     }
     void telemetry() {
         //Show Data for Specific Robot Mechanisms
-        telemetry.addData("Current upDown servo pos:", String.format("%.0f", servoPos * 255));
-        telemetry.addData("Current upDown servo pos:", String.format("%.0f", servoPos * 255));
+        telemetry.addData("Current leftClaw servo pos:", String.format("%.0f", clawServoPos * 255));
+        telemetry.addData("Current rightClaw servo pos:", String.format("%.0f", clawServoPos * 255));
+        telemetry.addData("Current lift motor power", String.format("%.0f", liftPower * 255));
     }
 
     //Create Methods that will update the driver data

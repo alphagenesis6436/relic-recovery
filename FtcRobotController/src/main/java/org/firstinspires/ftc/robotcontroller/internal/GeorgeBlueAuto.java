22ffb046a643d149cc29dd7b171af7a763d59bbc
Alex Arteaga <alexarte66@gmail.com>
2017-11-08 18:10:53 -0800

    modified GeorgeOp andGeorgeBlueAuto

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/GeorgeBlueAuto.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/GeorgeBlueAuto.java
@@ -1,8 +1,66 @@
 package org.firstinspires.ftc.robotcontroller.internal;
 
-/**
- * Created by Brooklyn on 11/8/2017.
- */
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-public class GeorgeBlueAuto {
+/**
+ * Created by Alex on 11/8/2017.
+ * Autonomous Objectives:
+ * --
+ */
+@Autonomous(name = "AutoTemplate", group = "default")
+@Disabled
+public class GeorgeBlueAuto extends GeorgeOp {
+    //Declare and Initialize any variables needed for this specific autonomous program
+
+
+    public GeorgeBlueAuto() {}
+
+    @Override
+    public void loop(){
+        //Display Data to be displayed throughout entire Autonomous
+        telemetry.addData(stateName, state);
+        telemetry.addData("current time", String.format("%.1f", this.time));
+        telemetry.addData("state time", String.format("%.1f", this.time - setTime));
+
+        //Use Switch statement to proceed through Autonomous strategy (only use even cases for steps)
+        switch(state){
+            case 0: //Use this state to reset all hardware devices
+                stateName = "Initial Calibration";
+                calibrateVariables();
+                resetEncoders();
+                state++;
+                break;
+
+            case 2: //Describe the Robot’s Goals & Actions in this state
+                stateName = "First State Goal";
+                //Display any current data needed to be seen during this state (if none is needed, omit this comment)
+
+
+                if (true) { //Use a boolean value that reads true when state goal is completed
+                    state++;
+                }
+                break;
+
+            case 1000: //Run When Autonomous is Complete
+                stateName = "Autonomous Complete";
+                //Set all motors to zero and servos to initial positions
+                calibrateVariables();
+                resetEncoders();
+                break;
+
+            default://Default state used to reset all hardware devices to ensure no errors
+                stateName = "Calibrating";
+                calibrateVariables();
+                resetEncoders();
+                if (waitSec(1)) {
+                    state++;
+                    setTime = this.time;
+                }
+                break;
+        }
+    }
+
+    //Create any methods needed for this specific autonomous program
+
 }
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/GeorgeOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/GeorgeOp.java
@@ -1,8 +1,232 @@
 package org.firstinspires.ftc.robotcontroller.internal;
 
-/**
- * Created by Brooklyn on 11/8/2017.
- */
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.util.Range;
 
-public class GeorgeOp {
+/**
+ * Created by Alex on 11/8/2017
+ * GeorgeOp is the TeleOp for the Competition Robot
+ * Has Following Robot Systems:
+ * --Mecanum Drive Train
+ * ----Follows Same Code from MecanumDriveOp
+ * ----Adds Gyro Sensor for Autonomous
+ * ----Adds turnAbsolute() and turnRelative() methods for autonomous
+ * ----Adds calibrateVariables() method for autonomous
+ * --Jewel Mechanism
+ * ----Follows Same Variables from PrototypeJewelOpMode3
+ * ----Modifies updateActuators() method [Renamed to updateJewel()] to accommodate Controls for other Systems
+ * ----Adds in the Color Sensor for Autonomous
+ */
+@TeleOp(name = "GeorgeOp", group = "Default")
+public class GeorgeOp extends OpMode {
+    //Declare any motors, servos, and sensors
+    DcMotor driveFR; //AndyMark, 40:1
+    DcMotor driveFL; //AndyMark, 40:1
+    DcMotor driveBR; //AndyMark, 40:1
+    DcMotor driveBL; //AndyMark, 40:1
+    Servo upDownServo; //Metal Gear, 180
+    Servo leftRightServo; //Metal Gear, 180
+    ColorSensor colorSensor; //For Jewel Mechanism
+    GyroSensor gyroSensor;
+    ModernRoboticsI2cGyro gyroMR; //For Mecanum Drive Train
+
+    //Mecanum Drive Train Variables and Constants
+    final double DRIVE_PWR_MAX = 0.90;
+    double forwardRightPower = 0;
+    double forwardLeftPower = 0;
+    double backwardRightPower = 0;
+    double backwardLeftPower = 0;
+
+    //Jewel Mechanism Variables and Constants
+    final float UPDOWN_MIN = 0 / 255.0f;
+    final float UPDOWN_MAX = 255 / 255.0f;
+    final float LEFTRIGHT_MIN = 0 / 255.0f;
+    final float LEFTRIGHT_MAX = 255 / 255.0f;
+    double upDownPos = 0;
+    double leftRightPos = 0;
+    double jewelDelta = 0.005;
+
+    public GeorgeOp() {}
+
+    @Override public void init() {
+        //Initialize motors & set direction
+        driveFR = hardwareMap.dcMotor.get("dfr");
+        driveFR.setDirection(DcMotorSimple.Direction.FORWARD);
+        driveFL = hardwareMap.dcMotor.get("dfl");
+        driveFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        driveBR = hardwareMap.dcMotor.get("dbr");
+        driveBR.setDirection(DcMotorSimple.Direction.FORWARD);
+        driveBL = hardwareMap.dcMotor.get("dbl");
+        driveBL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //Initialize servos
+        upDownServo = hardwareMap.servo.get("uds");
+        leftRightServo = hardwareMap.servo.get("lrs");
+
+        //Initialize sensors
+        colorSensor = hardwareMap.colorSensor.get("cs");
+        gyroSensor = hardwareMap.gyroSensor.get("gs");
+        gyroMR = (ModernRoboticsI2cGyro) gyroSensor;
+
+        telemetry();
+    }
+    @Override public void loop() {
+        //Update all the data based on driver input
+        updateData();
+
+     /* Clip Variables to make sure they don't exceed their
+      * ranged values and Set them to the Motors/Servos */
+        initialization();
+
+        //Show the Real Values of the Data Using Telemetry
+        telemetry();
+    }
+
+    void updateData() {
+        //Add in update methods for specific robot mechanisms
+        updateDriveTrain();
+        updateJewel();
+    }
+
+    void initialization() {
+        //Clip and Initialize Drive Train
+        forwardRightPower = Range.clip(forwardRightPower, -DRIVE_PWR_MAX, DRIVE_PWR_MAX);
+        driveFR.setPower(forwardRightPower);
+        forwardLeftPower = Range.clip(forwardLeftPower, -DRIVE_PWR_MAX, DRIVE_PWR_MAX);
+        driveFL.setPower(forwardLeftPower);
+        backwardRightPower = Range.clip(backwardRightPower, -DRIVE_PWR_MAX, DRIVE_PWR_MAX);
+        driveBR.setPower(backwardRightPower);
+        backwardLeftPower = Range.clip(backwardLeftPower, -DRIVE_PWR_MAX, DRIVE_PWR_MAX);
+        driveBL.setPower(backwardLeftPower);
+        //Clip and Initialize Jewel Mechanism
+        upDownPos = Range.clip(upDownPos, UPDOWN_MIN, UPDOWN_MAX);
+        upDownServo.setPosition(upDownPos);
+        leftRightPos = Range.clip(leftRightPos, LEFTRIGHT_MIN, LEFTRIGHT_MAX);
+        leftRightServo.setPosition(leftRightPos);
+    }
+    void telemetry() {
+        //Show Data for Specific Robot Mechanisms
+        telemetry.addData("FR", String.format("%.2f",forwardRightPower));
+        telemetry.addData("FL", String.format("%.2f",forwardLeftPower));
+        telemetry.addData("BR", String.format("%.2f",backwardRightPower));
+        telemetry.addData("BL", String.format("%.2f",forwardLeftPower));
+        telemetry.addData("UD", String.format("%.0f", upDownPos * 255));
+        telemetry.addData("LR", String.format("%.0f", leftRightPos * 255));
+        telemetry.addData("Gyro", gyroMR.getIntegratedZValue());
+        telemetry.addData("Red", colorSensor.red());
+        telemetry.addData("Blue", colorSensor.blue());
+    }
+
+    //Create Methods that will update the driver data
+    void updateDriveTrain() {
+        forwardRightPower = (-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) * DRIVE_PWR_MAX;
+        forwardLeftPower = (-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x) * DRIVE_PWR_MAX;
+        backwardRightPower = (-gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x) * DRIVE_PWR_MAX;
+        backwardLeftPower = (-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x) * DRIVE_PWR_MAX;
+    }
+    void updateJewel() {
+        if (gamepad2.dpad_up)
+            upDownPos += jewelDelta;
+        else if (gamepad2.dpad_down)
+            upDownPos -= jewelDelta;
+        if (gamepad2.dpad_right)
+            leftRightPos += jewelDelta;
+        else if (gamepad2.dpad_left)
+            leftRightPos -= jewelDelta;
+    }
+
+
+    //Create variables/methods that will be used in ALL autonomous programs for this specific robot
+
+    double setTime; //used to measure the time period of each step in autonomous
+    int state = 0; //used to control the steps taken during autonomous
+    String stateName = ""; //Overwrite this as the specific step used in Autonomous
+
+    void resetEncoders() {
+        driveFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    void runConstantSpeed() {
+        driveFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        driveFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        driveBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        driveBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    void runConstantPower() {
+        driveFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+    void stopDriveMotors() {
+        driveFR.setPower(0.0);
+        driveFL.setPower(0.0);
+        driveBR.setPower(0.0);
+        driveBL.setPower(0.0);
+    }
+    void moveForward(double power) {
+        driveFR.setPower(power);
+        driveFL.setPower(power);
+        driveBR.setPower(power);
+        driveBL.setPower(power);
+    }
+    void moveRight(double power) {
+        driveFR.setPower(-power);
+        driveFL.setPower(power);
+        driveBR.setPower(power);
+        driveBL.setPower(-power);
+    }
+    void moveForwardRight(double power) {
+        driveFR.setPower(0.0);
+        driveFL.setPower(power);
+        driveBR.setPower(power);
+        driveBL.setPower(0.0);
+    }
+    void moveForwardLeft(double power) {
+        driveFR.setPower(power);
+        driveFL.setPower(0.0);
+        driveBR.setPower(0.0);;
+        driveBL.setPower(power);
+    }
+    void turnClockwise(double power) {
+        driveFR.setPower(-power);
+        driveFL.setPower(power);
+        driveBR.setPower(-power);
+        driveBL.setPower(power);
+    }
+
+    boolean turnAbsolute(double target) { //Tells robot to rotate to an absolute heading (degrees)
+        boolean absoluteReached = false;
+        if (Math.abs(gyroMR.getIntegratedZValue() + target) <= Math.abs(driveFR.getPower() * 95)) {
+            stopDriveMotors();
+            absoluteReached = true;
+        }
+        return absoluteReached;
+    }
+    boolean turnRelative(int target) { //Tells robot to rotate target degrees from starting position
+        boolean relativeReached = false;
+        target -= startingIntZVal;
+        startingIntZVal = 0;
+        if (Math.abs(gyroMR.getIntegratedZValue() + target) <= Math.abs(driveFR.getPower() * 95)) {
+            stopDriveMotors();
+            relativeReached = true;
+        }
+        return relativeReached;
+    }
+    int startingIntZVal = 0; //Reset this value to last IntegratedZValue during each Calibrating State
+
+
+    void calibrateVariables() {//Used if any autonomous methods need initial state variables
+        startingIntZVal = gyroMR.getIntegratedZValue();
+    }
+    //used to measure the amount of time passed since a new step in autonomous has started
+    boolean waitSec(double elapsedTime) { return (this.time - setTime >= elapsedTime); }
+
 }
+

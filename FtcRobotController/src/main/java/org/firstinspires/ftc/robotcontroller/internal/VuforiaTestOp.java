dd861a1f9951356aba6b75eb86ec52a6835cf376
Alex Arteaga <alexarte66@gmail.com>
2017-11-11 13:00:03 -0800

    completed VuforiaTestOp ready for testing

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/VuforiaTestOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/VuforiaTestOp.java
@@ -7,9 +7,19 @@ import com.qualcomm.robotcore.hardware.*;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import static com.sun.tools.javac.util.Constants.format;
 
 /**
  * Created by Alex on 11/11/2017.
@@ -19,7 +29,18 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 @Disabled
 public class VuforiaTestOp extends APIKey {
     //Declare any objects for Vuforia
+    OpenGLMatrix lastLocation = null;
     VuforiaLocalizer vuforia;
+    VuforiaTrackables relicTrackables;
+    VuforiaTrackable relicTemplate;
+    double tX; // X value extracted from our the offset of the traget relative to the robot.
+    double tZ; // Same as above but for Z
+    double tY; // Same as above but for Y
+    // -----------------------------------
+    double rX; // X value extracted from the rotational components of the tartget relitive to the robot
+    double rY; // Same as above but for Y
+    double rZ; // Same as above but for Z
+    int pictogrpahKey = -1; //Left = 0, Center = 1, Right = 2
 
     public VuforiaTestOp() {}
 
@@ -32,8 +53,8 @@ public class VuforiaTestOp extends APIKey {
         parameters.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES; // Display Axes
 
         this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        relicTemplate = relicTrackables.get(0);
 
         //Initialize motors & set direction
 
@@ -42,6 +63,9 @@ public class VuforiaTestOp extends APIKey {
         //Initialize sensors
 
         telemetry();
+    }
+    @Override public void start() {
+        relicTrackables.activate();
     }
     @Override public void loop() {
         //Update all the data based on driver input
@@ -57,7 +81,7 @@ public class VuforiaTestOp extends APIKey {
 
     void updateData() {
         //Add in update methods for specific robot mechanisms
-
+        updateVuforia();
     }
 
     void initialization() {
@@ -78,6 +102,54 @@ public class VuforiaTestOp extends APIKey {
          //Step 2: (Physical Instructions on how to control specific robot mechanism using controller buttons)
          //Step ...: (Physical Instructions on how to control specific robot mechanism using controller buttons)
   */
+
+    void updateVuforia() {
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        if (vuMark != RelicRecoveryVuMark.UNKNOWN) { // Test to see if image is visable
+            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose(); // Get Positional value to use later
+            telemetry.addData("Pose", format(pose));
+            if (pose != null)
+            {
+                VectorF trans = pose.getTranslation();
+                Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+                // Extract the X, Y, and Z components of the offset of the target relative to the robot
+                tX = trans.get(0);
+                tY = trans.get(1);
+                tZ = trans.get(2);
+
+                // Extract the rotational components of the target relative to the robot. NOTE: VERY IMPORTANT IF BASING MOVEMENT OFF OF THE IMAGE!!!!
+                rX = rot.firstAngle;
+                rY = rot.secondAngle;
+                rZ = rot.thirdAngle;
+            }
+            if (vuMark == RelicRecoveryVuMark.LEFT)
+            { // Test to see if Image is the "LEFT" image and display value.
+                telemetry.addData("VuMark is", "Left");
+                telemetry.addData("X =", tX);
+                telemetry.addData("Y =", tY);
+                telemetry.addData("Z =", tZ);
+                pictogrpahKey = 0;
+            } else if (vuMark == RelicRecoveryVuMark.RIGHT)
+            { // Test to see if Image is the "RIGHT" image and display values.
+                telemetry.addData("VuMark is", "Right");
+                telemetry.addData("X =", tX);
+                telemetry.addData("Y =", tY);
+                telemetry.addData("Z =", tZ);
+                pictogrpahKey = 2;
+            } else if (vuMark == RelicRecoveryVuMark.CENTER)
+            { // Test to see if Image is the "CENTER" image and display values.
+                telemetry.addData("VuMark is", "Center");
+                telemetry.addData("X =", tX);
+                telemetry.addData("Y =", tY);
+                telemetry.addData("Z =", tZ);
+                pictogrpahKey = 1;
+            }
+        } else
+        {
+            telemetry.addData("VuMark", "not visible");
+        }
+    }
 
 
     //Create variables/methods that will be used in ALL autonomous programs for this specific robot

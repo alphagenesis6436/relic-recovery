e156afc803a41702a0a12445d1294ecada9456b1
Alex Arteaga <alexarte66@gmail.com>
2017-11-20 13:37:22 -0800

    changed COUNTS_PER_REVOLUTION from 1440 to 1120 for andymarkencoders

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/GeorgeOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/GeorgeOp.java
@@ -42,7 +42,7 @@ public class GeorgeOp extends OpMode {
     //Mecanum Drive Train Variables and Constants
     final double DRIVE_PWR_MAX = 0.70;
     final double TURN_PWR_MAX = 0.70;
-    final int COUNTS_PER_REVOLUTION = 1440; //AndyMark Motors
+    final int COUNTS_PER_REVOLUTION = 1120; //AndyMark Motors
     final double DRIVE_GEAR_RATIO = 16.0 / 24.0; //Driven / Driver
     final double COUNTS_PER_INCH_RF = COUNTS_PER_REVOLUTION / (4 * Math.PI / DRIVE_GEAR_RATIO); //forward / right / backward / left
     final double COUNTS_PER_INCH_DG = COUNTS_PER_REVOLUTION / (2 * Math.PI * Math.sqrt(2) / DRIVE_GEAR_RATIO); //diagonal
